621667515b8d0c8273fa57cd16f709c7d62f51dc
yuliiacherevata <yuliiacherevata@alamy.com>
2023-10-30 16:37:24 +0200

    fix: Fix cors issue

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './filters/exception-filter';
+import { NextFunction } from 'express';
 
 async function bootstrap() {
   const port = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+  app.use(function (request: Request, response: Response, next: NextFunction) {
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    next();
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
 
